865b542a856490d3875e35e8576377e65282c5f8
unknown <atomsiwawut@gmail.com>
2024-07-13 11:01:11 +0700

    “Update“

--- src/app/(routes)/stock/page.tsx
+++ src/app/(routes)/stock/page.tsx
@@ -1,25 +1,142 @@
 "use client";
 import * as React from "react";
-import { DataGrid, GridColDef, GridValueGetter } from "@mui/x-data-grid";
-
-const columns: GridColDef[] = [
-  { field: "id", headerName: "ID", width: 70 },
-  { field: "firstName", headerName: "First name", width: 130 },
-  { field: "lastName", headerName: "Last name", width: 130 },
-];
-
-const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-];
+import {
+  DataGrid,
+  GridColDef,
+  GridRenderCellParams,
+  GridToolbarContainer,
+  GridToolbarFilterButton,
+  GridValueGetter,
+} from "@mui/x-data-grid";
+import { useSelector } from "react-redux";
+import { getProducts, productSelector } from "@/src/store/slices/productSlice";
+import { useAppDispatch } from "@/src/store/store";
+import Image from "next/image";
+import { productImageURL } from "@/src/utils/commonUtil";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
+import { Fab, IconButton, Link, Stack, Typography } from "@mui/material";
+import { NumericFormat } from "react-number-format";
+import dayjs from "dayjs";
+import { Add, Delete, Edit } from "@mui/icons-material";
+import { useRouter } from "next/navigation";
 
 export default function Stock() {
+  const productReducer = useSelector(productSelector);
+  const dispatch = useAppDispatch();
+  const router = useRouter();
+
+  const columns: GridColDef[] = [
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "image",
+      headerName: "Image",
+      width: 70,
+      renderCell: ({ value }: GridRenderCellParams<String>) => (
+        <Zoom>
+          <Image
+            key={value}
+            height={500}
+            width={500}
+            alt="product image"
+            src={productImageURL(value)}
+            style={{
+              width: 70,
+              height: 70,
+              borderRadius: "5%",
+              objectFit: "cover",
+            }}
+          />
+        </Zoom>
+      ),
+    },
+    { field: "name", headerName: "Name", width: 350 },
+    {
+      field: "price",
+      headerName: "Price",
+      width: 130,
+      renderCell: ({ value }: GridRenderCellParams<String>) => (
+        <Typography variant="body1">
+          <NumericFormat
+            value={value}
+            displayType={"text"}
+            thousandSeparator={true}
+            decimalScale={0}
+            fixedDecimalScale={true}
+          />
+        </Typography>
+      ),
+    },
+    { field: "stock", headerName: "Stock", width: 130 },
+    {
+      field: "createdAt",
+      headerName: "Timestamp",
+      width: 230,
+      renderCell: ({ value }) => (
+        <Typography variant="body1">
+          {dayjs(value).format("DD/MM/YYYY HH:mm")}
+        </Typography>
+      ),
+    },
+    {
+      headerName: "ACTION",
+      field: ".",
+      width: 120,
+      renderCell: ({ row }: GridRenderCellParams<any>) => (
+        <Stack direction="row">
+          <IconButton
+            aria-label="edit"
+            size="large"
+            onClick={() => router.push(`/stock/edit?id=${row.id}`)}
+          >
+            <Edit fontSize="inherit" />
+          </IconButton>
+          <IconButton
+            aria-label="delete"
+            size="large"
+            onClick={() => {
+              // setSelectedProduct(row);
+              // setOpenDialog(true);
+            }}
+          >
+            <Delete fontSize="inherit" />
+          </IconButton>
+        </Stack>
+      ),
+    },
+  ];
+
+  React.useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  const CustomToolbar: React.FunctionComponent<{
+    setFilterButtonEl: React.Dispatch<
+      React.SetStateAction<HTMLButtonElement | null>
+    >;
+  }> = ({ setFilterButtonEl }) => (
+    <GridToolbarContainer>
+      <GridToolbarFilterButton ref={setFilterButtonEl} />
+      <Link href="/stock/add">
+        <Fab
+          color="primary"
+          aria-label="add"
+          sx={{
+            position: "absolute",
+            top: 10,
+            right: 10,
+          }}
+        >
+          <Add />
+        </Fab>
+      </Link>
+    </GridToolbarContainer>
+  );
+
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={rows}
+        rows={productReducer.products}
         columns={columns}
         initialState={{
           pagination: {
@@ -27,6 +144,9 @@ export default function Stock() {
           },
         }}
         pageSizeOptions={[5, 10]}
+        slots={{
+          toolbar: CustomToolbar,
+        }}
       />
     </div>
   );
